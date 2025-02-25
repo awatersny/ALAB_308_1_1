150ce519f1104a18f7c42ea1c73eff795770bb8d
awatersny <awatersny@gmail.com>
2025-02-25 16:19:10 -0500

    Check if all numbers are divisible by 5. Cache the result in a variable.

--- index.js
+++ index.js
@@ -46,10 +46,11 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
 
 // Part 1: Math Problems
 //TODO Implement the following:
-//TODO Check if all numbers are divisible by 5. Cache the result in a variable.
-//TODO Check if the first number is larger than the last. Cache the result in a variable.
-//TODO Accomplish the following arithmetic chain:
-//TODO Subtract the first number from the second number.
-//TODO Multiply the result by the third number.
-//TODO Find the remainder of dividing the result by the fourth number.
-//TODO Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
+//  Check if all numbers are divisible by 5. Cache the result in a variable.
+let isAllDivisibleBy5 = n1%5 && n2%5 && n3%5 && n4%5
+//  TODO Check if the first number is larger than the last. Cache the result in a variable.
+//  TODO Accomplish the following arithmetic chain:
+//    TODO Subtract the first number from the second number.
+//    TODO Multiply the result by the third number.
+//    TODO Find the remainder of dividing the result by the fourth number.
+//  TODO Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
